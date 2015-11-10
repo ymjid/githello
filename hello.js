2877a75c40fc0ae4d788bf5b6a3285705866fd9c
Yan <M.JID.Yannis@gmail.com>
2015-11-10 14:21:18 +0100

    Modification de  hello.js : Affichage des messages dans la div prévu à cet effet

--- hello.js
+++ hello.js
@@ -2,8 +2,14 @@ function hello (){
 	var name = prompt ("Indiquez le nom de la personne à saluer");
 	var nb = prompt("Indiquez le nombre de 'Hello' à afficher");
 	var i = 0;
+	var msg = "Hello " + name;
+	msgdiv = document.getElementById('msg');
 	while (i<nb){
-	 alert ('Hello ' + name);
+	 newmsg = document.createElement("p");
+	 newmsgtxt = document.createTextNode(msg);
+	 msgdiv.appendChild(newmsg);
+	 newmsg.appendChild(newmsgtxt);		 
 	 i++;
         }
+	
 }
