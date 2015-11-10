2ded9a2eaa42fb728cb49627bec6d3c5db376c43
Yan <M.JID.Yannis@gmail.com>
2015-11-10 14:41:13 +0100

    Modification de  hello.js : Ajout de message d'erreurs

--- hello.js
+++ hello.js
@@ -1,15 +1,37 @@
 function hello (){
-	var name = prompt ("Indiquez le nom de la personne à saluer");
-	var nb = prompt("Indiquez le nombre de 'Hello' à afficher");
-	var i = 0;
-	var msg = "Hello " + name;
 	msgdiv = document.getElementById('msg');
-	while (i<nb){
-	 newmsg = document.createElement("p");
-	 newmsgtxt = document.createTextNode(msg);
-	 msgdiv.appendChild(newmsg);
-	 newmsg.appendChild(newmsgtxt);		 
-	 i++;
-        }
+	var name = prompt ("Indiquez le nom de la personne a saluer");
+	if (name != null){
+		var nb = prompt("Indiquez le nombre de 'Hello' a afficher");
+	}
+	else {
+		var errormsg1 = "Aucun nom n'a été renseigné";
+		newmsg = document.createElement("p");
+		newmsg.setAttribute("class", "errormsg");
+         	newmsgtxt = document.createTextNode(errormsg1);
+         	msgdiv.appendChild(newmsg);
+         	newmsg.appendChild(newmsgtxt);
+		return;
+	}
+	if (nb != null) {
+		var i = 0;
+		var msg = "Hello " + name;
+		while (i<nb){
+	 		newmsg = document.createElement("p");
+	 		newmsgtxt = document.createTextNode(msg);
+	 		msgdiv.appendChild(newmsg);
+	 		newmsg.appendChild(newmsgtxt);		 
+	 		i++;
+        	}
+	}
+	else {
+                var errormsg1 = "Aucun nombre n'a été renseigné";
+                newmsg = document.createElement("p");
+		newmsg.setAttribute("class", "errormsg");
+                newmsgtxt =  document.createTextNode(errormsg1);
+                msgdiv.appendChild(newmsg);
+                newmsg.appendChild(newmsgtxt);
+		return;
+	}
 	
 }
